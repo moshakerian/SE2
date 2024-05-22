83e1acfdce67319d45480c46406596441f6e227e
Mozhdeh Shakerian <89453139+moshakerian@users.noreply.github.com>
2024-05-22 14:22:36 +0330

    Update momo.py

--- momo.py
+++ momo.py
@@ -2,4 +2,4 @@ num1 = float ( input ("enter the first number : "))
 num2 = float ( input ("enter the second number : "))
 average = (num1 + num2)/2
 print(" the average is : ", average)
-## the changes added in develope branch
+## the changes added in develope branch ( baraye tamrin emtiazie 8 !)
