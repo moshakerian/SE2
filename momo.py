56e116daa762386df10d623dbbadd75f85c14fef
Mozhdeh <shakerian.mozhdeh@>
2024-05-22 13:06:27 +0330

    the production of dev branch

--- momo.py
+++ momo.py
@@ -2,3 +2,4 @@ num1 = float ( input ("enter the first number : "))
 num2 = float ( input ("enter the second number : "))
 average = (num1 + num2)/2
 print(" the average is : ", average)
+## the changes added in develope branch
